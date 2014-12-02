d507014fc2dd5df7f7a7e2bcd30a249b58c8c6f7
Ruben Daniels <ruben@c9.io>
2014-12-02 11:11:46 +0100

    Apply gecko mask correctly for flat theme

--- flat-light.js
+++ flat-light.js
@@ -16,7 +16,7 @@ define(function(require, exports, module) {
         var plugin = new Plugin("Ajax.org", main.consumes);
         var emit = plugin.getEmitter();
         
-        var oldHeight, oldMinimizedHeight, oldTabInteraction;
+        var oldHeight, oldMinimizedHeight, oldTabInteraction, svg;
         
         var loaded = false;
         function load() {
@@ -43,12 +43,16 @@ define(function(require, exports, module) {
                     
                     tabinteraction.plusMargin = oldTabInteraction;
                 }
+                
+                setGeckoMask();
             };
             
             layout.on("themeChange", update);
             
             if (layout.theme == "flat-light")
                 update({ theme: layout.theme });
+            
+            setGeckoMask();
         }
         
         var drawn = false;
@@ -60,6 +64,37 @@ define(function(require, exports, module) {
         }
         
         /***** Methods *****/
+        
+        // There will be a better place for this when theming is fully
+        // abstracted. For now this is a hack
+        function setGeckoMask(){
+            if (!apf.isGecko) return;
+            
+            if (svg) svg.parentNode.removeChild(svg);
+            
+            var isFlatTheme = layout.theme.indexOf("flat") > -1;
+            var img = "/static/plugins/c9.ide.layout.classic/images/" + (
+                isFlatTheme
+                    ? "gecko_mask_flat_light.png"
+                    : "gecko_mask.png");
+            var width = isFlatTheme ? 76 : 46;
+            var height = isFlatTheme ? 26 : 24;
+            var x1 = isFlatTheme ? 1 : 1;
+            var x2 = isFlatTheme ? -40 : -28;
+            
+            document.body.insertAdjacentHTML("beforeend", '<svg xmlns="http://www.w3.org/2000/svg">'
+                + '<defs>'
+                    + '<mask id="tab-mask-left" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse">'
+                        + '<image width="' + width + 'px" height="' + height + 'px" xlink:href="' + img + '" x="' + x1 + 'px"></image>'
+                    + '</mask>'
+                    + '<mask id="tab-mask-right" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse">'
+                        + '<image width="' + width + 'px" height="' + height + 'px" xlink:href="' + img + '" x="' + x2 + 'px"></image>'
+                    + '</mask>'
+                + '</defs>'
+            + '</svg>');
+            
+            svg = document.body.lastChild;
+        }
         
         /***** Lifecycle *****/
         
@@ -75,6 +110,7 @@ define(function(require, exports, module) {
         plugin.on("unload", function() {
             loaded = false;
             drawn = false;
+            svg.parentNode.removeChild(svg);
         });
         
         /***** Register and define API *****/
